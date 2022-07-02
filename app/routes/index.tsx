ecb85c2aa5031eadb74c45b59a287a7ada5385e1
Veeti K <veeti@veetik.com>
2022-07-02 21:48:24 +0300

    coming soon

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-900">
+      <h1 className="font-bold text-3xl text-slate-100">Coming soon</h1>
+    </div>
+  );
+}
